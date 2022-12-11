44fdaac72c2262b0d76eeaeb07a92082a6a22a6d
iluvadev <76512587+iluvadev@users.noreply.github.com>
2022-12-11 20:50:22 +0100

    Clean dependences, Publish to win,linux,os

--- src/PocketBaseClient.CodeGenerator/Program.cs
+++ src/PocketBaseClient.CodeGenerator/Program.cs
@@ -12,7 +12,7 @@ using pocketbase_csharp_sdk;
 using pocketbase_csharp_sdk.Models;
 using pocketbase_csharp_sdk.Models.Collection;
 using PocketBaseClient.CodeGenerator.Models;
-using System.CommandLine;
+//using System.CommandLine;
 using System.Text.Json;
 
 namespace PocketBaseClient.CodeGenerator
@@ -41,121 +41,119 @@ namespace PocketBaseClient.CodeGenerator
  An application to generate client side code in c# for your PocketBase application
 ";
 
-
-
-
-        static async Task<int> Main(string[] args)
+        static int Main(string[] args)
         {
-            if (args.Length == 0)
+            //if (args.Length == 0)
             {
                 Interactive.Process.Start();
                 return 0;
             }
 
-            var rootCommand = new RootCommand("PocketBaseClient Code Generator: An application to generate client side code of your PocketBase application");
+            ////Nuget: System.CommandLine
+            //var rootCommand = new RootCommand("PocketBaseClient Code Generator: An application to generate client side code of your PocketBase application");
 
-            rootCommand.AddCommand(GetInteractiveCommand());
-            rootCommand.AddCommand(GetSaveschemaCommand());
+            //rootCommand.AddCommand(GetInteractiveCommand());
+            //rootCommand.AddCommand(GetSaveschemaCommand());
 
-            // Commands:
-            //  * saveschema --url --email --pwd --file
-            //  * generate --schema --format --folder
+            //// Commands:
+            ////  * saveschema --url --email --pwd --file
+            ////  * generate --schema --format --folder
 
-            //TODO: Do it in own option, not Hardcoded!!
-            //GenerateCode(file.FullName, @"C:\Dev\iluvadev\projects\PocketBaseClient-csharp\src\PocketBaseClient.SampleApp", "PocketBaseClient.SampleApp");
+            ////TODO: Do it in own option, not Hardcoded!!
+            ////GenerateCode(file.FullName, @"C:\Dev\iluvadev\projects\PocketBaseClient-csharp\src\PocketBaseClient.SampleApp", "PocketBaseClient.SampleApp");
 
-            return await rootCommand.InvokeAsync(args);
+            //return await rootCommand.InvokeAsync(args);
         }
 
-        #region Commands
-        private static Command GetSaveschemaCommand()
-        {
-            var urlOption = new Option<Uri?>(
-                name: "--url",
-                description: "The url of your PocketBase application",
-                getDefaultValue: () => new Uri("http://127.0.0.1:8090"));
+        //#region Commands
+        //private static Command GetSaveschemaCommand()
+        //{
+        //    var urlOption = new Option<Uri?>(
+        //        name: "--url",
+        //        description: "The url of your PocketBase application",
+        //        getDefaultValue: () => new Uri("http://127.0.0.1:8090"));
 
-            var emailOption = new Option<string>(
-                name: "--email",
-                description: "email to login with Admin rights in your PocketBase application");
+        //    var emailOption = new Option<string>(
+        //        name: "--email",
+        //        description: "email to login with Admin rights in your PocketBase application");
 
-            var pwdOption = new Option<string>(
-                name: "--pwd",
-                description: "Password to login with Admin rights in your PocketBase application");
+        //    var pwdOption = new Option<string>(
+        //        name: "--pwd",
+        //        description: "Password to login with Admin rights in your PocketBase application");
 
-            var fileOption = new Option<FileInfo?>(
-                name: "--file",
-                description: "The file where download the schema information of your application",
-                parseArgument: result =>
-                {
-                    if (result.Tokens.Count == 0)
-                    {
-                        result.ErrorMessage = "File does not set";
-                        return null;
-                    }
-                    string? filePath = result.Tokens.Single().Value;
-                    if (!File.Exists(filePath))
-                    {
-                        result.ErrorMessage = "File does not exist";
-                        return null;
-                    }
-                    return new FileInfo(filePath);
-                });
+        //    var fileOption = new Option<FileInfo?>(
+        //        name: "--file",
+        //        description: "The file where download the schema information of your application",
+        //        parseArgument: result =>
+        //        {
+        //            if (result.Tokens.Count == 0)
+        //            {
+        //                result.ErrorMessage = "File does not set";
+        //                return null;
+        //            }
+        //            string? filePath = result.Tokens.Single().Value;
+        //            if (!File.Exists(filePath))
+        //            {
+        //                result.ErrorMessage = "File does not exist";
+        //                return null;
+        //            }
+        //            return new FileInfo(filePath);
+        //        });
 
-            var commandDownloadSchema = new Command("saveschema", "Download the schema of your PocketBase application")
-            {
-                urlOption,
-                emailOption,
-                pwdOption,
-                fileOption
-            };
+        //    var commandDownloadSchema = new Command("saveschema", "Download the schema of your PocketBase application")
+        //    {
+        //        urlOption,
+        //        emailOption,
+        //        pwdOption,
+        //        fileOption
+        //    };
 
-            commandDownloadSchema.SetHandler(async (url, email, pwd, file) => await SchemaDownloader.DownloadSchemaAsync(url!, email!, pwd!, file!),
-                                             urlOption, emailOption, pwdOption, fileOption);
-            return commandDownloadSchema;
-        }
+        //    commandDownloadSchema.SetHandler(async (url, email, pwd, file) => await SchemaDownloader.DownloadSchemaAsync(url!, email!, pwd!, file!),
+        //                                     urlOption, emailOption, pwdOption, fileOption);
+        //    return commandDownloadSchema;
+        //}
 
-        private static Command GetInteractiveCommand()
-        {
-            var commandInteractive = new Command("i", "Executes PocketBaseClient Code Generator in interactive mode");
-            commandInteractive.SetHandler(Interactive.Process.Start);
-            return commandInteractive;
-        }
-        #endregion Commands
+        //private static Command GetInteractiveCommand()
+        //{
+        //    var commandInteractive = new Command("i", "Executes PocketBaseClient Code Generator in interactive mode");
+        //    commandInteractive.SetHandler(Interactive.Process.Start);
+        //    return commandInteractive;
+        //}
+        //#endregion Commands
 
-        private static async void GetTestData(PocketBaseClientApplication app, string path)
-        {
-            Console.WriteLine($"Getting text data...");
-            var pagedUsers = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/users/records", HttpMethod.Get);
-            var pagedTestTypes = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/test_for_types/records", HttpMethod.Get);
-            var pagedTestRel = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/test_for_related/records", HttpMethod.Get);
-            var all = new { Users = pagedUsers?.Items, TestTypes = pagedTestTypes?.Items, TestRel = pagedTestRel?.Items };
-            var jsonString = JsonSerializer.Serialize(all);
-            File.WriteAllText(path, jsonString);
-            Console.WriteLine($"Done!");
-        }
+        //private static async void GetTestData(PocketBaseClientApplication app, string path)
+        //{
+        //    Console.WriteLine($"Getting text data...");
+        //    var pagedUsers = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/users/records", HttpMethod.Get);
+        //    var pagedTestTypes = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/test_for_types/records", HttpMethod.Get);
+        //    var pagedTestRel = await app.Sdk.SendAsync<PagedCollectionModel<object>>($"/api/collections/test_for_related/records", HttpMethod.Get);
+        //    var all = new { Users = pagedUsers?.Items, TestTypes = pagedTestTypes?.Items, TestRel = pagedTestRel?.Items };
+        //    var jsonString = JsonSerializer.Serialize(all);
+        //    File.WriteAllText(path, jsonString);
+        //    Console.WriteLine($"Done!");
+        //}
 
-        private static async Task<PagedCollectionModel<object>> ListAsync(PocketBaseClientApplication app, string collection,
-            int? page = null, int? perPage = null, string? sort = null, string? filter = null, string? expand = null, IDictionary<string, string>? headers = null)
-        {
-            var query = new Dictionary<string, object?>()
-            {
-                { "filter", filter },
-                { "page", page },
-                { "perPage", perPage },
-                { "sort", sort },
-                { "expand", expand },
-            };
-            var url = $"/api/collections/{collection}/records";
-            var pagedCollection = await app.Sdk.SendAsync<PagedCollectionModel<object>>(
-                url,
-                HttpMethod.Get,
-                headers: headers,
-                query: query);
-            if (pagedCollection is null) throw new ClientException(url);
+        //private static async Task<PagedCollectionModel<object>> ListAsync(PocketBaseClientApplication app, string collection,
+        //    int? page = null, int? perPage = null, string? sort = null, string? filter = null, string? expand = null, IDictionary<string, string>? headers = null)
+        //{
+        //    var query = new Dictionary<string, object?>()
+        //    {
+        //        { "filter", filter },
+        //        { "page", page },
+        //        { "perPage", perPage },
+        //        { "sort", sort },
+        //        { "expand", expand },
+        //    };
+        //    var url = $"/api/collections/{collection}/records";
+        //    var pagedCollection = await app.Sdk.SendAsync<PagedCollectionModel<object>>(
+        //        url,
+        //        HttpMethod.Get,
+        //        headers: headers,
+        //        query: query);
+        //    if (pagedCollection is null) throw new ClientException(url);
 
-            return pagedCollection;
-        }
+        //    return pagedCollection;
+        //}
 
 
     }
