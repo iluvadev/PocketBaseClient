0435aab533db915287473fe61e72e5eefccda03e
iluvadev <iluvadev@gmail.com>
2023-02-04 20:44:57 +0100

    Pure sync GetById in Items

--- src/Test/PocketBaseClient.DemoTest/Models/Category.cs
+++ src/Test/PocketBaseClient.DemoTest/Models/Category.cs
@@ -59,6 +59,9 @@ namespace PocketBaseClient.DemoTest.Models
         #endregion Collection
 
         public static async Task<Category?> GetByIdAsync(string id, bool reload = false)
-            => await DataServiceBase.GetCollection<Category>()!.GetByIdAsync(id, reload);
+            => await GetCollection().GetByIdAsync(id, reload);
+
+        public static Category? GetById(string id, bool reload = false)
+            => GetCollection().GetById(id, reload);
     }
 }
--- src/Test/PocketBaseClient.DemoTest/Models/TestForRelated.cs
+++ src/Test/PocketBaseClient.DemoTest/Models/TestForRelated.cs
@@ -77,6 +77,9 @@ namespace PocketBaseClient.DemoTest.Models
         #endregion Collection
 
         public static async Task<TestForRelated?> GetByIdAsync(string id, bool reload = false)
-            => await DataServiceBase.GetCollection<TestForRelated>()!.GetByIdAsync(id, reload);
+            => await GetCollection().GetByIdAsync(id, reload);
+
+        public static TestForRelated? GetById(string id, bool reload = false)
+            => GetCollection().GetById(id, reload);
     }
 }
