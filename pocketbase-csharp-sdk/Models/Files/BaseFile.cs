bf6b99f58d84cfac1f988297ba2d91a4f8e9e778
iluvadev <iluvadev@gmail.com>
2023-02-11 18:36:16 +0100

    Squashed 'src/pocketbase-csharp-sdk/' content from commit 0b00609
    git-subtree-dir: src/pocketbase-csharp-sdk
    git-subtree-split: 0b00609a9f8655eb1d429671944fe7aca7e8f2df

---
+++ pocketbase-csharp-sdk/Models/Files/BaseFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pocketbase_csharp_sdk.Models.Files
+{
+    public abstract class BaseFile
+    {
+        public string? FieldName { get; set; }
+        public string? FileName { get; set; }
+    }
+}
