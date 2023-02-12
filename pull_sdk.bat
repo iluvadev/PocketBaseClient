a7b734dc7d84e30b6369282747df2a9638c2d4ca
iluvadev <iluvadev@gmail.com>
2023-02-12 17:08:41 +0100

    sdk subtree in dir sdk

--- pull_sdk.bat
+++ pull_sdk.bat
@@ -1,3 +1,3 @@
-git subtree pull --prefix ./src/pocketbase-csharp-sdk https://github.com/iluvadev/pocketbase-csharp-sdk.git master --squash
+git subtree pull --prefix sdk https://github.com/iluvadev/pocketbase-csharp-sdk.git master --squash
 
 pause
