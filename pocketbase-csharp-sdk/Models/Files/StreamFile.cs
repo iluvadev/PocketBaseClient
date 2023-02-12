5219616ca5839484f3ee8d8d3701aa2aab732292
iluvadev <iluvadev@gmail.com>
2023-02-12 17:02:48 +0100

    Squashed 'sdk/' content from commit 0b00609
    git-subtree-dir: sdk
    git-subtree-split: 0b00609a9f8655eb1d429671944fe7aca7e8f2df

---
+++ pocketbase-csharp-sdk/Models/Files/StreamFile.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pocketbase_csharp_sdk.Models.Files
+{
+
+    /// <summary>
+    /// simple class for uploading files to PocketBase, accepting a Stream
+    /// </summary>
+    public class StreamFile : BaseFile, IFile
+    {
+        public Stream? Stream { get; set; }
+
+        public Stream? GetStream()
+        {
+            return Stream;
+        }
+
+        public StreamFile()
+        {
+            
+        }
+
+        public StreamFile(Stream? stream)
+        {
+            this.Stream = stream;
+        }
+
+    }
+}
